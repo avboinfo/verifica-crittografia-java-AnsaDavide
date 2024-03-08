967a02ea7b9d33a494238930f45f2e92ec1d8a44
AnsaDavide <150423454+AnsaDavide@users.noreply.github.com>
2024-03-08 09:51:20 +0000

    Modifiche

---
+++ src/Cryto.java
@@ -0,0 +1,26 @@
+public class Cryto {
+
+    static protected String Cesar(String data, int shift) {
+        StringBuilder output = new StringBuilder(data.length());
+        for (char singleChar : data.toCharArray()) {
+            int shiftedChar = (int) (singleChar + shift) % 256;
+            output.append((char) shiftedChar);
+        }
+        return output.toString();
+    }
+
+    static protected String xorAlgo(String data, String key) {
+
+        String acc = "";
+
+        for (int i = 0; i < data.length(); i++) {
+            char d = data.charAt(i);
+                char k = key.charAt(i%key.length());
+
+                char xorChar = (char) (d ^ k);
+                acc = acc + xorChar;
+        }
+
+        return acc;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String testo = readFile("Dati.txt"); 
+        String testoCrittografato = Cryto.Cesar(testo, 5);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Inserire una chiave: ");
+        String chiave = in.nextLine();
+        String xor = Cryto.xorAlgo(testoCrittografato, chiave);
+        System.out.println(xor);
+        in.close();
+
+  }
+
+  public static String readFile(String filename) {
+    String acc = "";
+    try {
+      File myObj = new File(filename);
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        acc += data;
+      }
+      myReader.close();
+      return acc;
+    } catch (FileNotFoundException e) {
+      System.out.println("Potrebbe esserci un errore");
+      e.printStackTrace();
     }
+  return null;
+    }
+
+    public static void saveFile(String filename, String data) {
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
+    writer.append(data);
+    writer.close();
+    } catch (IOException e) {
+      System.out.println("Potrebbe esserci un errore");
+      e.printStackTrace();
+    }
+  }
+
+
 }
